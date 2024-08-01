81eed3ac4c361cf1ccbbff0fe3eca0591e80c3e5
Lacasse <audrey-anne.lacasse@agr.gc.ca>
2024-08-01 13:04:39 -0400

    Remove submodel

--- 1. Main.R
+++ 1. Main.R
@@ -1,13 +1,12 @@
 #Set location, initial date and end time; date origin in R, 1970-1-1
 
 test <- 1  #the test number
-Location <- "VAC" #VA, VAC, or LA
+Location <- "VA" #VA, VAC, or LA
 
-for (submodels in 0:1) { #1 with cover, 0 is without cover
 source("3. Parameters.R",echo = F)  #Parameters we can change
 source("4. Constants.R",echo = F)   #Constants no need to change
 #The major loop for original model
 source("2. Major loop.R",echo = F)
-}
+
 source("Result comparison.R",echo = F)
 source("stat output.R", echo =  F)
